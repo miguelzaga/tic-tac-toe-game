bb999ca34ef03fd0e071ef388cfe6a7f9fbfb531
Miguel <miguezaga2@hotmail.com>
2022-08-31 18:47:35 -0500

    add grid to gameboard and update prettier command

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./scss/style.scss"
+import "./scss/style.scss";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
