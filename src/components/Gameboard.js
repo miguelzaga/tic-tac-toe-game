89175a2a1097a0fb5c9dfe6b77b041fa12343eac
Miguel <miguezaga2@hotmail.com>
2022-09-01 18:11:39 -0500

    style gameboard footer

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -56,15 +56,15 @@ function Gameboard() {
         </button>
       </main>
       <footer className="game__score">
-        <div className="game__score-x">
+        <div className="game__score-cell game__score-cell--1">
           <h2 className="game__score-name">X (You)</h2>
           <p className="game__score-num">14</p>
         </div>
-        <div className="game__score-t">
+        <div className="game__score-cell game__score-cell--2">
           <h2 className="game__score-name">Ties</h2>
           <p className="game__score-num">32</p>
         </div>
-        <div className="game__score-o">
+        <div className="game__score-cell game__score-cell--3">
           <h2 className="game__score-name">O (Cpu)</h2>
           <p className="game__score-num">11</p>
         </div>
